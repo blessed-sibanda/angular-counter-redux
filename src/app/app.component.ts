22f9f500102968d344ab5f94d5274f6e6c4a2b8b
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-12-18 15:08:49 +0200

    wire up redux store with app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { Store } from 'redux';
+import { AppState } from './app.state';
+import { AppStore } from './app.store';
+import CounterActions from './counter.actions';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'counter-redux';
+  counter: number | undefined;
+
+  constructor(@Inject(AppStore) private store: Store<AppState>) {
+    store.subscribe(() => this.readState());
+    this.readState();
+  }
+
+  readState() {
+    const state: AppState = this.store.getState() as AppState;
+    this.counter = state.counter;
+  }
+
+  increment() {
+    this.store.dispatch(CounterActions.increment());
+  }
+
+  decrement() {
+    this.store.dispatch(CounterActions.decrement());
+  }
 }
--- src/app/counter.actions.ts
+++ src/app/counter.actions.ts
@@ -3,10 +3,12 @@ import { ActionCreator, AnyAction } from 'redux';
 export const INCREMENT: string = 'INCREMENT';
 export const DECREMENT: string = 'DECREMENT';
 
-export const increment: ActionCreator<AnyAction> = () => ({
-  type: INCREMENT,
-});
+export default class CounterActions {
+  static increment: ActionCreator<AnyAction> = () => ({
+    type: INCREMENT,
+  });
 
-export const decrement: ActionCreator<AnyAction> = () => ({
-  type: DECREMENT,
-});
+  static decrement: ActionCreator<AnyAction> = () => ({
+    type: DECREMENT,
+  });
+}
