db31babc396a116a6f98829384699b932a985482
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-12-18 14:52:11 +0200

    provide the app store using DI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { appStoreProviders } from './app.store';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule],
+  providers: [appStoreProviders],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/app.store.ts
+++ src/app/app.store.ts
@@ -1,3 +1,4 @@
+import { InjectionToken } from '@angular/core';
 import { createStore, Store } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { AppState } from './app.state';
@@ -6,3 +7,9 @@ import { counterReducer } from './counter.reducer';
 export function createAppStore(): Store<AppState> {
   return createStore(counterReducer, composeWithDevTools());
 }
+
+export const AppStore = new InjectionToken('App.store');
+
+export const appStoreProviders = [
+  { provide: AppStore, useFactory: createAppStore },
+];
